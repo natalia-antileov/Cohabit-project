163c6d5c62eea374b44704e40d5f9bccf59b2ede
natalia-antileov <166961502+natalia-antileov@users.noreply.github.com>
2025-11-02 02:21:11 -0300

    Update DashboardHeader.tsx

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -6,7 +6,7 @@ interface DashboardHeaderProps {
 
 export const DashboardHeader: React.FC<DashboardHeaderProps> = ({ userName }) => {
   return (
-    <header className="bg-[rgba(0,110,111,1)] z-0 flex w-full flex-col overflow-hidden text-[32px] text-white font-extrabold leading-none pt-[54px] pb-[100px] px-[19px]">
+    <header className="bg-[rgba(0,110,111,1)] z-0 flex w-full flex-col overflow-hidden text-[32px] text-white font-extrabold leading-none pt-[54px] pb-[50px] px-[19px]">
       <h1 className="mb-[-31px]">
         Hola, {userName}
       </h1>
